c78c5a7a15e16929c3de3d1febddc9b68ac0e6b4
Kaarel Kaljurand <kaljurand@gmail.com>
2012-08-31 11:44:05 +0200

    Add CNL 2012 slides

---
+++ files/cnl2012_speech_slides/slide_config.js
@@ -0,0 +1,27 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Controlled natural language<br>in speech recognition based user interfaces',
+    //subtitle: 'Subtitle Goes Here',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: false, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Kaarel Kaljurand',
+    company: 'Institute of Computational Linguistics, University of Zurich'
+  }, {
+    name: 'Tanel Alumäe',
+    company: 'Institute of Cybernetics, Tallinn University of Technology'
+  }]
+};
