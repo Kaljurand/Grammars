c50dc86a569c454d54ba634ee7b74781113d7df3
Kaarel Kaljurand <kaljurand@gmail.com>
2014-06-21 20:45:05 +0200

    Improve test-and-diff.sh

--- gf/test-and-diff.sh
+++ gf/test-and-diff.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
 
-test_out_dir=build/test_out
-test_gold_dir=test_gold
+# Extracts jsgf- and dict-files from the given pgf-files
+# and/or compares the extracted files with their (previous) versions
+# in the given directory.
+# Also, runs the test.sh-files in the directories that correspond to the pgf-files
+# (if they exist).
+
+test_out_dir=
+test_gold_dir=
+
+# Reset in case getopts has been used previously in the shell.
+OPTIND=1
+
+function show_help {
+    echo "Usage examples:
+
+Compare the two given directories, ignoring certain files which
+contain machine-only code.
+
+    ./test-and-diff.sh -f build/test_out/ -g test_gold/
+
+Generate various outputs into the given directory based on the given PGF-files.
+
+    ./test-and-diff.sh -f build/test_out/ build/pgf/*.pgf
+"
+}
+
+while getopts "h?f:g:" opt; do
+    case "$opt" in
+    h|\?)
+        # TODO: define it
+        show_help
+        exit 0
+        ;;
+    f)  test_out_dir=$OPTARG
+        ;;
+    g)  test_gold_dir=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
 
 path="${GF_EST_SRC}/estonian:../lib:../Address:../Alarm:../ArithExpr:../Calc:../Date:../Dial:../Digit:../Direction:../Eeppl:../Expr:../Fraction:../Letter:../Number:../Tallinnaddress:../Unitconv:../Weather"
 
-# Extracts jsgf- and dict-files from the pgf-files in this directory.
-# Compares the extracted files with their previous versions.
-# Runs the test.sh-files in the directories that correspond to the pgf-files.
 jsgf=jsgf
 dict=dict
 
@@ -16,10 +54,8 @@ tools=../tools/
 jsgf_langs="Eng Est"
 dict_langs="Est"
 
-rm -r ${test_out_dir}
-
 time_start=`date +%s`
-for pgf in *.pgf; do
+for pgf in "$@"; do
 	name=$(basename "$pgf")
 	ext="${name##*.}"
 	name="${name%.*}"
@@ -57,7 +93,12 @@ for pgf in *.pgf; do
 		fi
 	done
 done
-diff -r -x *.fsm -x *.fsg -x *.sym ${test_gold_dir} ${test_out_dir}
+
+if [ -n "${test_gold_dir}" ]
+then
+    diff -r -x *.fsm -x *.fsg -x *.sym ${test_gold_dir} ${test_out_dir}
+fi
+
 time_end=`date +%s`
 time_elapsed=$((time_end - time_start))
 echo "Duration: $time_elapsed sec (" $(( time_elapsed / 60 ))m $(( time_elapsed % 60 ))s ")"
